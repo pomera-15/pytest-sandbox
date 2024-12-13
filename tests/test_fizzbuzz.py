4e8577be036777e0a8034c1cda0459372358d332
pomera-15 <shoheiyamanashi1204@gmail.com>
2024-12-13 09:37:52 +0900

    リファクタリング

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -8,43 +8,23 @@ def fizzbuzz():
     return fizzbuzz
 
 
-def test_fizzbuzz_3の倍数でFizzを返す(fizzbuzz):
+def test_3の倍数でFizzを返す(fizzbuzz):
     assert fizzbuzz.convert(3) == "Fizz"
 
 
-def test_fizzbuzz_5の倍数でBuzzを返す(fizzbuzz):
-    fizzbuzz = FizzBuzz()
+def test_5の倍数でBuzzを返す(fizzbuzz):
     assert fizzbuzz.convert(5) == "Buzz"
 
 
-def test_fizzbuzz_3の倍数かつ5の倍数の場合でFizzBuzzを返す(fizzbuzz):
-    fizzbuzz = FizzBuzz()
+def test_3の倍数かつ5の倍数の場合でFizzBuzzを返す(fizzbuzz):
     assert fizzbuzz.convert(15) == "FizzBuzz"
 
 
-def test_fizzbuzz_それ以外の場合はnを返す(fizzbuzz):
-    fizzbuzz = FizzBuzz()
+def test_それ以外の場合はnを返す(fizzbuzz):
     assert fizzbuzz.convert(7) == "7"
 
 
-def test_fizzbuzz_引数がint型でない場合にValueErrorを発生させる(fizzbuzz):
-    fizzbuzz = FizzBuzz()
+def test_引数がint型でない場合にValueErrorを発生させる(fizzbuzz):
     with pytest.raises(ValueError) as e:
         fizzbuzz.convert("a")
-
     assert str(e.value) == "引数がint型ではありません。"
-
-
-# classを使う場合の書き方
-class Test_3の倍数の場合:
-    def test_3でFizzを返す(self):
-        fizzbuzz = FizzBuzz()
-        assert fizzbuzz.convert(3) == "Fizz"
-
-    def test_6でFizzを返す(self):
-        fizzbuzz = FizzBuzz()
-        assert fizzbuzz.convert(6) == "Fizz"
-
-    def test_9でFizzを返す(self):
-        fizzbuzz = FizzBuzz()
-        assert fizzbuzz.convert(9) == "Fizz"
